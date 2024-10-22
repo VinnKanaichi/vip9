8a3ec6465e1ea88e0530712457ab617531e8a653
VinnKanaichi <jodijunio22@gmail.com>
2024-10-22 22:56:40 +0700

    vip9

---
+++ settings.js
@@ -0,0 +1,12 @@
+// OBEY COMMANDMENTS
+
+global.prefa = ['','!','.',',','🐤','🗿'] //NOT CHANGE
+global.owner = ['628388198229'] //OWNER
+global.botname = 'BITCH!' //BOT NAME
+global.versisc = "5.0.0" //NOT CHANGE
+global.baileys1 = require('@whiskeysockets/baileys') //NOT CHANGE
+global.creator = "Tama_Ultraa" //NOT CHANGE
+global.packname = "Sticker By" //OPSIONAL
+global.author = "Tama_Ultraa" //OPSIONAL
+global.simbol = "" // SIMBOL MENU
+global.idCH = "0012345678998765432100@newsletter"
